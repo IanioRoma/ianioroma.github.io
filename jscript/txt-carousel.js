af5996f548207409e67b62a9aa86436e84df9b94
Roman Ianioglo <105991268+IanioRoma@users.noreply.github.com>
2023-05-30 20:06:16 +0100

    Update txt-carousel.js
    update

--- jscript/txt-carousel.js
+++ jscript/txt-carousel.js
@@ -1,5 +1,5 @@
 var typingEffect = new Typed(".multiText",{
-  strings : ["Website", "SEO optimization", "Social Media Marketing"],
+  strings : ["Website", "SEO", "Social Media Marketing"],
   loop : true,
   typeSpeed : 80,
   backSpeed : 50,
