66670874c558e584bcc87f688fe7941f3163379b
Roman Ianioglo <105991268+IanioRoma@users.noreply.github.com>
2023-08-05 23:09:28 +0100

    Update txt-carousel.js
    removing SMM

--- jscript/txt-carousel.js
+++ jscript/txt-carousel.js
@@ -1,5 +1,5 @@
 var typingEffect = new Typed(".multiText",{
-  strings : ["Website", "SEO", "Social Media Marketing"],
+  strings : ["Website", "SEO"],
   loop : true,
   typeSpeed : 80,
   backSpeed : 50,
