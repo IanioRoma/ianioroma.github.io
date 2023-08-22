260e64ee7f60a49c2369cb9a547112de2d4d06cf
Roman Ianioglo <105991268+IanioRoma@users.noreply.github.com>
2023-08-22 19:24:28 +0100

    Update form_submission.js
    Fixing fetch error

--- js/form_submission.js
+++ js/form_submission.js
@@ -1,26 +1,29 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Client-side validation
-  if (!this.checkValidity()) {
-      event.stopPropagation();
-      this.classList.add("was-validated");
-      return;
-  }
+    // Client-side validation
+    if (!this.checkValidity()) {
+        event.stopPropagation();
+        this.classList.add("was-validated");
+        return;
+    }
 
-  const formData = new FormData(this);
+    const formData = new FormData(this);
 
-  // Send form data to server using AJAX
-  fetch("process-form.php", {
-      method: "POST",
-      body: formData
-  })
-  .then(response => response.json())
-  .then(data => {
-      console.log(data); // Response from the server
-      // Display success or error message to the user
-  })
-  .catch(error => {
-      console.error("Error:", error);
-  });
+    // Specify the AJAX URL here
+    const ajaxUrl = "http://formsubb.000.pe/process-form.php";
+
+    // Send form data to server using AJAX
+    fetch(ajaxUrl, {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Response from the server
+        // Display success or error message to the user
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
 });
