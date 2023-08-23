e5cc6ef17932cc8fed161689f508f7faff9b7369
Roman Ianioglo <105991268+IanioRoma@users.noreply.github.com>
2023-08-23 20:31:31 +0100

    Update txt-carousel.js
    removing TypeEffect error

--- js/txt-carousel.js
+++ js/txt-carousel.js
@@ -8,19 +8,8 @@ try {
   console.error("An error occurred while initializing the typing effect:", error);
 }
 
-try {
-var typingEffect = new Typed(".portfolio",{
-  strings : ["Portfolio"],
-  loop : false,
-  typeSpeed : 80,
-  backSpeed : 50,
-  backDelay : 1500
-});
-} catch (error) {
-  console.error("An error occurred while initializing the typing effect:", error);
-}
 /* var typingEffect = new Typed(".multiText",{
-  strings : ["Website", "SEO optimisation", "Social Media Marketing"],
+  strings : ["Website", "SEO optimisation"],
   loop : true,
   typeSpeed : 80,
   backSpeed : 50,
